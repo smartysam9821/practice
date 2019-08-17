8492538aa24fe48a8091457cb53f3f3a76012c75
Shyam Yadav <shyamyadav@Shyams-MacBook-Air.local>
2019-08-17 21:44:52 +0530

    qestion 2nd added

--- file1.py
+++ file1.py
@@ -2,6 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 a = np.random.randint(10,100,50)
+#1. crate boxplot for numpy array
+plt.boxplot(a)
+plt.show()
+
+
+# 3. create histogram for numpy array
 plt.hist(a)
+plt.show()
 
 print("histogram plotted")
+
